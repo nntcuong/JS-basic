cf17b705d0133199594ab1102c5dc3e8e98789d1
nntcuong <thecuongyt3htk@gmail.com>
2023-09-21 15:40:28 +0700

    bai5

--- bai4/bai4.js
+++
@@ -1,9 +0,0 @@
-let a='Toi la chuoi';
-console.log(a);
-console.log(4+5+'a');
-console.log('a'+4+5);
-let b=`i'm cuong`;
-console.log(b);
-let c=`im "viking" 
-yes`;
-console.log(c);
---
+++ bai5/bai5.js
@@ -0,0 +1,5 @@
+let a=5,b=10;
+
+console.log('Fifteen is'+(a+b)+' and\nnot'+ (2*a+b)+'.');
+
+console.log(`Check strings: a=${a}`);
