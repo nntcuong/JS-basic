5587aff334e078a3bed3c50b28eeb0ee993d1bd5
nntcuong <thecuongyt3htk@gmail.com>
2023-09-21 17:11:12 +0700

    bai6

--- bai5/bai5.js
+++ bai5/bai5.js
@@ -1,5 +1,20 @@
-let a=5,b=10;
+console.log('Hello world from HTML');
 
-console.log('Fifteen is'+(a+b)+' and\nnot'+ (2*a+b)+'.');
+//key: value
+let obj = {
+    name: 'Eric',
+    address: 'Ha Noi',
+    email: '',
+    gender: ''
+};
 
-console.log(`Check strings: a=${a}`);
+
+
+
+let b = 'address';
+
+// obj[b] = 'Hary'
+obj.name = 'HCM'
+
+console.log('what is your name ? ', `My name 's : `, obj['name'], obj)
+console.log('Where are you from ? ', `I'm from : `, obj['address'])
