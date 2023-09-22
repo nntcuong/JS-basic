a53677d70cd184852147b51cb520e868ef98f8d2
nntcuong <thecuongyt3htk@gmail.com>
2023-09-22 23:21:01 +0700

    swith case

--- bai8/bai8.js
+++ bai8/bai8.js
@@ -1,15 +1,26 @@
-console.log('Hello world from HTML')
+console.log('Hello world with HTML')
 
-let arrTop4 = ['Live', 'Chelsea', 'Mu', 'Man city']
-
-let i = 0;
-while (i < arrTop4.length) {
-    i++;
-    if (arrTop4[i] === 'Chelsea') {
-        console.log('Found it: ', arrTop4[i])
-        continue;
-    }
-
-    console.log('>>> check i: ', i)
-
+let day = '';
+let a = 2;
+switch (a) {
+    case 0:
+    case 1:
+        day = "Monday";
+        break;
+    case 2:
+        day = "Tuesday";
+        break;
+    case 3:
+        day = "Wednesday";
+        break;
+    case 4:
+        day = "Thursday";
+        break;
+    case 5:
+        day = "Friday";
+        break;
+    case 6:
+        day = "Saturday";
 }
+
+console.log('check day: ', day, new Date().getDay());
