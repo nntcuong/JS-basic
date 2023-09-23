5b9afad5304da7586655048085370f009ba414d1
nntcuong <thecuongyt3htk@gmail.com>
2023-09-23 23:21:47 +0700

    Callbacks

--- bai8/bai8.js
+++ bai8/bai8.js
@@ -1,11 +1,24 @@
 console.log('Hello world from HTML')
 
-// function sum(a, b) {
-//     return a + b;
-// }
-//arrow function 
-let sum = (a, b) => {
-    return a + b;
+let sum = (a, b, callback) => {
+    let tong = a + b;
+    setTimeout(() => {
+        callback(tong);
+    }, 5000) // miliseconds
+    let i = 0;
+    let timer = setInterval(() => {
+        callback(tong);
+        i++;
+        if (i === 5) {
+            clearInterval(timer)
+        }
+    }, 1000);
+
 }
 
-console.log(' check sum: ', sum(6, 9))
+
+let printSum = (message) => {
+    console.log('>> check sum: 6 + 9 = ', message)
+}
+
+sum(6, 9, printSum)
